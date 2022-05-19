e7f48eff88a8924d0bc53dd01584243af387bec7
Ayobami <aajanaku1@gmail.com>
2022-05-19 10:04:40 +0100

    Update: Passing the date dynamically to get the date attached to the banner image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { LandingPageContext } from '../../pages/LandingPage/LandingPage'
 
 const Header = () => {
+  const {state:{imageInfo}} = useContext(LandingPageContext)
+
+  const d = imageInfo ? imageInfo.date : ''
+  let newDate = ''
+
+  const getDate = (date)=>{
+    if (date){
+      newDate = new Date(d)
+      console.log(newDate)
+    }
+  }
+  
+  getDate(d)
   return (
     <header className='text-center py-2.5'>
         <h1 className='text-primary-font text-xl font-bold'> 
             <span className='text-default-font'>NASA: </span>Picture of the Day
         </h1>
-        <h2 className='text-primary-font text-xl font-bold'>Wednesday, May 18, 2022</h2>
+        <h2 className='text-primary-font text-xl font-bold'></h2>
     </header>
   )
 }
